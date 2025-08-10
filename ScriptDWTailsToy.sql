1236b3c5fcde7755cb10b3a98ec450ab77de3738
ErikaYass <na15005@ues.edu.sv>
2025-08-10 14:42:37 -0600

    Creación de DIM STATE
    Creación de la dimensión STATE correspondiente a la DW

--- ScriptDWTailsToy.sql
+++ ScriptDWTailsToy.sql
@@ -32,3 +32,15 @@ CREATE TABLE dw.dim_product (
     retail_price MONEY NOT NULL
 );
 GO
+
+-- Creación de DIM STATE
+
+CREATE TABLE dw.dim_state (
+    state_sk INT IDENTITY(1,1) PRIMARY KEY,
+    state_bk INT NOT NULL, -- StateID del origen
+    state_code NVARCHAR(2) NOT NULL,
+    state_name NVARCHAR(50) NOT NULL,
+    time_zone NVARCHAR(10) NOT NULL,
+
+);
+GO
