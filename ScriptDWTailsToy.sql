377fb8fc5d66287012a06325ecf7bee343828592
ErikaYass <na15005@ues.edu.sv>
2025-08-10 14:51:04 -0600

    Creación de FACT SALES
    Creación de tabla FACT SALES, la cual representará el núcleo de nuestra DW

--- ScriptDWTailsToy.sql
+++ ScriptDWTailsToy.sql
@@ -54,3 +54,25 @@ CREATE TABLE dw.dim_region (
 
 );
 GO
+
+-- Creación de tabla FACT SALES
+
+CREATE TABLE dw.fact_sales (
+    sales_id BIGINT IDENTITY(1,1) PRIMARY KEY,
+    order_number NCHAR(10) NOT NULL,
+    date_key INT NOT NULL,  -- FK a dim_date
+    product_sk INT NOT NULL, -- FK a dim_date
+    state_sk INT NOT NULL,-- FK a dim_date
+    region_sk INT NOT NULL,-- FK a dim_date
+    quantity INT NOT NULL,
+    unit_price DECIMAL(9,2) NOT NULL,
+    discount_amount DECIMAL(9,2) NOT NULL,
+    total_amount AS (quantity * unit_price - discount_amount) PERSISTED,
+
+    CONSTRAINT fk_fact_date FOREIGN KEY (date_key) REFERENCES dw.dim_date(date_key),
+    CONSTRAINT fk_fact_product FOREIGN KEY (product_sk) REFERENCES dw.dim_product(product_sk),
+    CONSTRAINT fk_fact_state FOREIGN KEY (state_sk) REFERENCES dw.dim_state(state_sk),
+    CONSTRAINT fk_fact_region FOREIGN KEY (region_sk) REFERENCES dw.dim_region(region_sk),
+    
+);
+GO
